75db4492366b42fe6f409fc25e1eb8d6915999e0
mingchu <mingchu@suneee.com>
2019-02-11 16:57:56 +0800

    springboot starter commit

---
+++ SpringBootStarter/src/main/java/com/hong/spring_abc/springboot/SpringBootStarter/App.java
@@ -0,0 +1,13 @@
+package com.hong.spring_abc.springboot.SpringBootStarter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
