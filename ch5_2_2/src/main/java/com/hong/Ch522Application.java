cecd3f89caee3b4740fb445dc97b13fad70883d5
anye2009 <anye2009@aliyun.com>
2020-10-22 20:34:31 +0800

    add demo project

---
+++ ch5_2_2/src/main/java/com/hong/Ch522Application.java
@@ -0,0 +1,15 @@
+package com.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Ch522Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch522Application.class, args);
+	}
+
+}
