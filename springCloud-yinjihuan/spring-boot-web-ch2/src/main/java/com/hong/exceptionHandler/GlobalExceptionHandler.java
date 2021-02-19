fc382593ef9cf780b48d934be85af22859e4d139
anye <anye2009@aliyun.com>
2021-02-19 16:39:18 +0800

    统一返回结果定义-yinjihuan

---
+++ springCloud-yinjihuan/spring-boot-web-ch2/src/main/java/com/hong/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.hong.exceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResponseData defaultErrorHandler(HttpServletRequest request, Exception e)
+        throws Exception {
+        ResponseData r = new ResponseData();
+        logger.error("", e);
+        r.setMessage(e.getMessage());
+        if (e instanceof org.springframework.web.servlet.NoHandlerFoundException) {
+            r.setCode(4040);
+        } else {
+            r.setCode(5000);
+        }
+        r.setData(null);
+        r.setStatus(false);
+        return r;
+    }
+}
---
+++ springCloud-yinjihuan/spring-boot-web-ch2/src/main/java/com/hong/exceptionHandler/ResponseData.java
@@ -0,0 +1,5 @@
+package com.hong.exceptionHandler;
+
+public class ResponseData {
+
+}
