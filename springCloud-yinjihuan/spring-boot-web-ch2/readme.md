1ad815ff41286f757c0f6199391faea4b33ad74e
hongwanzhen <hongwanzhen@cmss.chinamobile.com>
2021-02-25 11:22:50 +0800

    commit md

--- springCloud-yinjihuan/spring-boot-web-ch2/readme.md
+++ springCloud-yinjihuan/spring-boot-web-ch2/readme.md
@@ -1,1 +1,2 @@
+SpringCloud 微服务全栈技术 尹吉欢   
 CH2 springboot
--- springCloud-yinjihuan/spring-boot-web-ch2/src/main/java/com/hong/service/impl/HelloServiceImpl.java
+++ springCloud-yinjihuan/spring-boot-web-ch2/src/main/java/com/hong/service/impl/HelloServiceImpl.java
@@ -9,24 +9,24 @@ import com.hong.service.HelloService;
 
 @Service
 public class HelloServiceImpl implements HelloService {
-	@Autowired
-	private TraditionMultiThread traditionMultiThread;
-	@Autowired
-	private AsyncMultiThread asyncMultiThread;
+    @Autowired
+    private TraditionMultiThread traditionMultiThread;
+    @Autowired
+    private AsyncMultiThread asyncMultiThread;
 
-	@Override
-	public void testAsync() {
-		// 传统多线程
-		int i=0;
-		while(true) {
-		traditionMultiThread.testTraditionMultiThread();
-		i++;
-		if(i==50) {
-			break;
-		}
-		}
-		// @Async 多线程
-		asyncMultiThread.saveLog();
-	}
+    @Override
+    public void testAsync() {
+        // 传统多线程
+        int i = 0;
+        while (true) {
+            traditionMultiThread.testTraditionMultiThread();
+            i++;
+            if (i == 50) {
+                break;
+            }
+        }
+        // @Async 多线程
+        asyncMultiThread.saveLog();
+    }
 
 }
