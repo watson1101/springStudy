0cefee545ea08aa02ac3dd5ce9bf833175b7b951
watson1101 <makefoxrush@oultook.com>
2024-10-10 23:15:28 +0800

    move files

---
+++ file/demo/src/main/java/com/yy/domain/UserInfo.java
@@ -0,0 +1,60 @@
+package com.yy.domain;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+    private long id;
+    private String username;//帐号
+    private String name;//名称（昵称或者真实姓名，不同系统不同定义）
+    private String password; //密码;
+    private String salt;//加密密码的盐
+    private byte state;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public byte getState() {
+        return state;
+    }
+
+    public void setState(byte state) {
+        this.state = state;
+    }
+}
