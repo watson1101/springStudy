c898769929b4362b76255e213ca9140bf0879765
Watson1101 <anye2009@aliyun.com>
2025-05-10 06:03:44 +0800

    item-service

--- springStudy/item-service/src/main/java/com/hong/controller/ItemController.java
+++ springStudy/item-service/src/main/java/com/hong/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.hong.controller;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.hong.domain.ItemVO;
 import com.hong.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,8 +27,8 @@ public class ItemController {
     @PostMapping("/items")
     public String getItems(@RequestParam("ids") String ids) {
         log.info("get items by ids: {}", ids);
-        itemService.getItems(ids);
-        return null;
+        List<ItemVO> items = itemService.getItemsByIdString(ids);
+        return JSONObject.toJSONString(items);
     }
 
 
