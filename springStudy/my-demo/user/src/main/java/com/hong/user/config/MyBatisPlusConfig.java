24c7d462c0675f51ae63b7c620310a5bca72df36
Watson1101 <anye2009@aliyun.com>
2026-08-25 16:01:54 +0800

    refactor: 迁移 demo 模块到 microservice-learn，清理旧目录

--- springStudy/my-demo/user/src/main/java/com/hong/user/config/MyBatisPlusConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hong.user.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.hong.user.mapper")
-public class MyBatisPlusConfig {
-}
